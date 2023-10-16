871a5f49444ea87baa535ec053b84ad76303d923
Ravigopal Kesari <kesariravigopal@gmail.com>
2023-10-16 21:12:13 +0530

    Adds shadcn/ui library

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-// A fetch wrapper sample function
-export async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
-  const res = await fetch(input, init);
-
-  if (!res.ok) {
-    const json = await res.json();
-    if (json.error) {
-      const error = new Error(json.error) as Error & {
-        status: number;
-      };
-      error.status = res.status;
-      throw error;
-    } else {
-      throw new Error("An unexpected error occurred");
-    }
-  }
-  return res.json();
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
